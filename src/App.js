b9660b604392f1ffd64a64ecdee9e9f9d76efbb4
TAHAWE <amer.zp582@gmail.com>
2022-02-04 00:01:47 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+// import Article from "./componants/article/Article";
+// import Brand from "./componants/brand/Brand";
+// import CTA from "./componants/cta/CTA";
+// import Feature from "./componants/feature/Feature";
+// import Navbar from "./componants/navbar/Navbar";
+
+function App() {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componants/article/Article.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./arcticle.css";
+
+const Article = () => {
+  return <div>Article</div>;
+};
+
+export default Article;
