2820c45e1addb6f1cb8a4e12179083322403e93a
tahawei <amer.zp582@gmail.com>
2022-02-04 20:11:47 +0200

    add files

--- src/componants/navbar/Navbar.jsx
+++ src/componants/navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 // import { RiMenu3Line, RiCloseLin } from "./react-icons/ri";
+import logo from "../../assets/logo.svg";
 import "./navbar.css";
 const Navbar = () => {
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
-        <div className="gpt3__navbar-lonks_logo"></div>
+        <div className="gpt3__navbar-lonks_logo">
+          <img src={logo} alt="Logo" />
+        </div>
       </div>
     </div>
   );
