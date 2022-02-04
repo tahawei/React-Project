65a2e7b6be6313c3a6b9f2e9a491310e9af43de2
tahawei <amer.zp582@gmail.com>
2022-02-04 19:46:45 +0200

    edit 2

--- src/componants/navbar/Navbar.jsx
+++ src/componants/navbar/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+// import { RiMenu3Line, RiCloseLin } from "./react-icons/ri";
 import "./navbar.css";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-lonks_logo"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
