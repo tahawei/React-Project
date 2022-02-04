4e06c82e0d05b014c6f340e35ee9e8c6faea1333
tahawei <amer.zp582@gmail.com>
2022-02-04 15:44:47 +0200

    Edit 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import "./App.css";
-import { Article, Brand, CTA, Feature, Navbar } from "./componants";
+import { /*Article,*/ Brand, CTA, Feature, Navbar } from "./componants";
+import {
+  Blog,
+  // Features,
+  Footer,
+  // header,
+  Possibility,
+  WhatGBT3,
+} from "./Containers";
+import Header from "./Containers/header/Header";
 function App() {
   return (
-    <div>
-      <h1>App</h1>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGBT3 />
+      <Feature />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
---
+++ src/Containers/index.js
@@ -0,0 +1,6 @@
+export { default as Blog } from "./blog/Blog";
+export { default as Features } from "./features/Features";
+export { default as Footer } from "./footer/Footer";
+export { default as header } from "./header/Header";
+export { default as Possibility } from "./possibility/Possibility";
+export { default as WhatGBT3 } from "./whatGBT3/WhatGBT3";
